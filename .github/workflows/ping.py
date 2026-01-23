c296c678ed6dc4bbe9e75e53884588725d620af2
pyrocent <panzieri.ale+github@gmail.com>
2026-01-23 12:21:43 +0100

    commit

--- .github/workflows/ping.py
+++ .github/workflows/ping.py
@@ -2,6 +2,6 @@ from websockets import connect
 from asyncio import run, wait_for
 
 async def main():
-    async with connect("wss://api.playontable.com/websocket/") as websocket: wait_for(await websocket.ping(), 10)
+    async with connect("wss://api.playontable.com/websocket/") as websocket: await wait_for(await websocket.ping(), 10)
 
 run(main())
