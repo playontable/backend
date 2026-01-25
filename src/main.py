4b4d6b69627854ea2c8e4fe8bb27553636c5cda8
pyrocent <panzieri.ale+github@gmail.com>
2026-01-25 19:11:21 +0100

    commit

--- src/main.py
+++ src/main.py
@@ -13,6 +13,7 @@ class User:
     def __init__(self, websocket, /):
         while app.state.users.setdefault(code := "".join(choice("ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789") for _ in range(5)), self) is not self: pass
         self.code = code
+        self.play = False
         self.room = {self}
         self.websocket = websocket
 
@@ -26,12 +27,26 @@ class User:
         if app.state.users.get(self.code) is self: app.state.users.pop(self.code, None)
         await self.websocket.close()
 
-    async def broadcast(self, json, /, *, exclude = None): await self.websocket.send_json({"hook": "fail"}) if json.get("hook") == "room" and len(self.room) <= 1 else await gather(*(user.websocket.send_json(json) for user in self.room if user is not exclude), return_exceptions = True)
+    async def broadcast(self, json, /, *, exclude = None): await gather(*(user.websocket.send_json(json) for user in self.room if user is not exclude), return_exceptions = True)
 
 async def handle(user, json = None, /):
-    if (hook := json.get("hook")) != "join": await user.broadcast(json, exclude = user if hook in {"drag", "drop"} else None)
-    elif (host := app.state.users.get(json.get("data"))) is not None and host is not user:
-        for user in (merged := user.room | host.room): user.room = merged
+
+    hook = json.get("hook")
+
+    if hook == "join":
+        host = app.state.users.get(json.get("data"))
+        if host is not None:
+            if host.play: await user.websocket.send_json({"hook": "fail", "data": "join"})
+            elif host is not user:
+                for user in (merged := user.room | host.room): user.room = merged
+            else: await user.websocket.send_json({"hook": "fail", "data": "same"})
+        else: await user.websocket.send_json({"hook": "fail", "data": "room"})
+    elif hook == "room":
+        if len(user.room) <= 1: await user.websocket.send_json({"hook": "fail", "data": "only"})
+        else:
+            for user in user.room: user.play = True
+            await user.broadcast(json)
+    else: await user.broadcast(json, exclude = user if hook in {"drag", "drop"} else None)
 
 app = FastAPI(lifespan = lifespan, openapi_url = None)
 
