823bceabf4b27f57b3b453f5bc457c6d3a54d217
pyrocent <panzieri.ale+github@gmail.com>
2026-03-23 23:04:06 +0100

    commit

--- api/src/main.py
+++ api/src/main.py
@@ -58,7 +58,7 @@ async def websocket(websocket: WebSocket):
     async with User( websocket.app.state.manager, websocket) as user:
         async for json in websocket.iter_json():
             try: await handle(user, adapter.validate_python(json).model_dump())
-            except RoomError as fail: await user.websocket.send_json({"hook": "fail", "data": fail.reason})
+            except RoomError as fail: await user.websocket.send_json({"hook": "fail", "data": {"fail": fail.reason}})
             except ValidationError as info:
                 for error in info.errors(): logger.error("ValidationError\n\nUSER = %s\nJSON = %s\nINFO = %s\n\n", getattr(user, "websocket"), json, error["msg"])
 
